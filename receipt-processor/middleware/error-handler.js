856b137da3f014fe5a9d5098fd9c40ba71c2a5c1
vedant_42 <32434874+vedant-patel42@users.noreply.github.com>
2023-07-12 23:28:30 -0400

    code updated

--- receipt-processor/middleware/error-handler.js
+++ receipt-processor/middleware/error-handler.js
@@ -7,6 +7,9 @@ const errorHandlerMiddleware = (err,req,res,next) =>{
     else if(err instanceof SyntaxError && err.status==400 && 'body' in err){
         return res.status(400).json({message:'Invalid JSON in the request payload'});
     }
+    else if(err.message==='Empty receipt data'){
+        return res.status(400).json({ message: err.message });
+    }
     return res.status(500).json({message:'Try again'})
 }
 
